16a5e1f51d25ab65e40a4f3db4a1aa8ee7c606ea
ali <kemal_tar@hotmail.com>
2022-10-30 03:03:57 -0500

    new practice file is added

--- js/practice.js
+++ js/practice.js
@@ -1,1 +1,19 @@
-console.log("Hello")
+const items =[
+    {name:"Bike",price: 100},
+    {name:"TV",price: 200},
+    {name:"Album",price: 10},
+    {name:"Book",price: 5},
+    {name:"BookPhone",price: 500},
+    {name:"Phone",price: 1000},
+    {name:"Computer",price: 25},
+    
+];
+
+const itemNames=items.map((item)=>{
+    return item.price
+})
+//console.log(items)
+console.table(itemNames);
+
+// const lastitem=itemNames[itemNames.length-1];
+// console.log(lastitem)
